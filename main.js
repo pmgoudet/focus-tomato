c082e7692a432de601c3163bcae9796ea2e9bb5f
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-01-15 17:00:20 +0100

    .
    .

--- main.js
+++ main.js
@@ -1,78 +1,76 @@
-let html = document.querySelector('html');
-let contextItems = document.querySelectorAll('.context-item');
-let clock = document.querySelector('#clock');
-let startPauseBtn = document.querySelector('.start-pause-btn');
-let clockOn = false;
-
 // CHANGE CONTEXT
+const html = document.querySelector('html');
+const contextItems = document.querySelectorAll('.context-item');
+
 contextItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        html.setAttribute('data-context', item.dataset.context);
-    })
+  item.addEventListener('click', () => {
+    contextItems.forEach((item) => {
+      item.querySelector('button').classList.remove('active');
+    });
+    html.setAttribute('data-context', item.dataset.context);
+    item.querySelector('button').classList.add('active');
+  })
 })
 
 // CLOCK
-let timeInSeconds = 5;
 
-const countDown = () => {
-  if (timeInSeconds <= 0) {
-    reset();
-    return;
-  }
-  timeInSeconds -= 1;
-  console.log("Timer:" + timeInSeconds);
-};
+const display = document.querySelector('#clock');
+const startPauseBtn = document.querySelector('.start-pause-btn');
+const duration = 60;  // VOLTAR PARA 60
 
+let timer;
+let interval;
+let isPaused = true;
+let pomodoroCount = 0;
+let ciclesBeforeLongBreak = 3;
 
-
-
-
-
-
-
-
-
-/*
-
-
-let idInterval = null;
-
-
-
-startPauseBt.addEventListener("click", startPause);
-
-function startPause() {
-  if (idInterval) {
-    reset();
-    return;
-  }
-  idInterval = setInterval(countDown, 1000);
+function formatTime(seconds) { // time format
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return (
+    (minutes < 10 ? '0' : '') + minutes + ':' + (remainingSeconds < 10 ? '0' : '') + remainingSeconds
+  );
 }
 
-function reset() {
-  clearInterval(idInterval);
-  idInterval = null;
+function updateDisplay() { // actualize timer display
+  display.textContent = formatTime(timer);
 }
 
-const countDown = () => {
-  if (timeInSeconds <= 0) {
-    audioTempoFinalizado.play();
-    alert("Tempo finalizado");
-    reset();
-    return;
+function startPauseTimer() {
+  if (isPaused) {
+    // Start timer
+    isPaused = false;
+    startPauseBtn.innerHTML = `Pause <img src="assets/img/icons/pause-icon.svg" alt="Pause Icon">`;
+    interval = setInterval(() => {
+      if (timer > 0) {
+        timer--;
+        updateDisplay();
+      } else {
+        clearInterval(interval);
+        pomodoroCount++;
+        if (pomodoroCount < ciclesBeforeLongBreak) {
+          startPauseBtn.innerHTML = `Short Break?`;
+          let btnControl = true; // Play Btn Control after timer is finished
+          if (btnControl) {
+            startPauseBtn.addEventListener('click', () => {
+              html.setAttribute('data-context', 'short');
+              
+            });
+          }
+        } else {
+          // AQUI O BOTÃO PRA LONG BREAK
+        } 
+      }
+    }, 1000);
+  } else {
+    // Pause timer
+    isPaused = true;
+    startPauseBtn.innerHTML = `Start <img src="assets/img/icons/play-icon.svg" alt="Play Icon">`;
+    clearInterval(interval);
   }
-  timeInSeconds -= 1;
-  console.log("Tempo: " + timeInSeconds);
-  console.log("Id: " + idInterval);
-};
+}
 
-function startPause() {
-  if (idInterval) {
-    audioPausa.play();
-    reset();
-    return; // early return -- circuit breaker
-  }
+timer = duration;
+updateDisplay();
 
-
-
-*/
+startPauseBtn.addEventListener('click', startPauseTimer);
