004151f57791e6f1af380d9ccb5968b061c30c77
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-01-16 16:58:02 +0100

    ,m
    ,m

--- main.js
+++ main.js
@@ -2,6 +2,20 @@
 const html = document.querySelector('html');
 const contextItems = document.querySelectorAll('.context-item');
 
+// CLOCK
+const display = document.querySelector('#clock');
+const startPauseBtn = document.querySelector('.start-pause-btn');
+const duration = 1500;  // 60x minutes
+let startAudio = new Audio('/assets/sounds/start.mp3');
+let pauseAudio = new Audio('/assets/sounds/pause.mp3');
+let endAudio = new Audio('/assets/sounds/end.mp3');
+let timer;
+let interval;
+let isPaused = true;
+let pomodoroCount = 0;
+let ciclesBeforeLongBreak = 3;
+
+// CHANGE CONTEXT
 contextItems.forEach((item) => {
   item.addEventListener('click', () => {
     contextItems.forEach((item) => {
@@ -9,21 +23,24 @@ contextItems.forEach((item) => {
     });
     html.setAttribute('data-context', item.dataset.context);
     item.querySelector('button').classList.add('active');
+    startPauseBtn.innerHTML = `Start <img src="assets/img/icons/play-icon.svg" alt="Play Icon">`;
+    clearInterval(interval);
+    isPaused = true;
+
+    if (item.dataset.context == 'focus') { // display clock
+      display.textContent = '25:00';
+      timer = 1500;
+    } else if (item.dataset.context == 'short') {
+      display.textContent = '05:00';
+      timer = 300;
+    } else {
+      display.textContent = '15:00'
+      timer = 900;
+    }
   })
-})
+});
 
 // CLOCK
-
-const display = document.querySelector('#clock');
-const startPauseBtn = document.querySelector('.start-pause-btn');
-const duration = 60;  // VOLTAR PARA 60
-
-let timer;
-let interval;
-let isPaused = true;
-let pomodoroCount = 0;
-let ciclesBeforeLongBreak = 3;
-
 function formatTime(seconds) { // time format
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
@@ -41,31 +58,32 @@ function startPauseTimer() {
     // Start timer
     isPaused = false;
     startPauseBtn.innerHTML = `Pause <img src="assets/img/icons/pause-icon.svg" alt="Pause Icon">`;
+    startAudio.play();
     interval = setInterval(() => {
       if (timer > 0) {
         timer--;
         updateDisplay();
       } else {
+        endAudio.play();
         clearInterval(interval);
         pomodoroCount++;
         if (pomodoroCount < ciclesBeforeLongBreak) {
-          startPauseBtn.innerHTML = `Short Break?`;
-          let btnControl = true; // Play Btn Control after timer is finished
-          if (btnControl) {
+          startPauseBtn.textContent = `Good!`;
+          if (html.theme == "focus" && btnControl) {
             startPauseBtn.addEventListener('click', () => {
-              html.setAttribute('data-context', 'short');
-              
+              display.textContent = '05:00';
             });
           }
         } else {
           // AQUI O BOTÃO PRA LONG BREAK
-        } 
+        }
       }
     }, 1000);
   } else {
     // Pause timer
     isPaused = true;
     startPauseBtn.innerHTML = `Start <img src="assets/img/icons/play-icon.svg" alt="Play Icon">`;
+    pauseAudio.play();
     clearInterval(interval);
   }
 }
